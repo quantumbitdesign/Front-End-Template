87a5ff77f5bb1a0b04004668b0a0561cddba86fc
Kevin Goudswaard <kevingoudswaard@gmail.com>
2017-09-26 14:47:30 -0700

    Initialization

---
+++ script.js
@@ -0,0 +1,14 @@
+// Copyright 2014-2017 The Bootstrap Authors
+// Copyright 2014-2017 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+// The following code helps resolve a bug for IE Mobile Devices and Bootstrap
+//
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.head.appendChild(msViewportStyle)
+}
